4935bc0850f77a2b08e94986975551bd4f84394d
Patnaree Johnson <j.patnaree.t@gmail.com>
2023-10-12 15:59:07 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,1 +1,53 @@
 console.log('Write your code here');
+
+const exhibitTitle = document.querySelector('#exhibit-title');
+const exhibitDescription = document.getElementById('exhibit-description');
+const commentSection = document.getElementById('comments-section');
+const exhibitImage = document.getElementById('exhibit-image');
+const butTicketsButton = document.getElementById('buy-tickets-button');
+const ticketBought = document.getElementById('tickets-bought')
+let counter
+const commentForm = document.getElementById('comment-form')
+const commentInput = document.getElementById('comment-form')[0]
+
+function showArt(artpiece) {
+    exhibitTitle.textContent = artpiece.title;
+    exhibitImage.src = artpiece.image;
+    exhibitDescription.textContent = artpiece.description;
+    counter = artpiece.tickets_bought
+    ticketBought.textContent = `${counter} Tickets Bought`;
+
+    artpiece.comments.forEach(comment => addComment(comment))
+}
+
+function addComment(comment) {
+    const p = document.createElement('p');
+    p.textContent = comment;
+    commentSection.appendChild(p)
+
+}
+
+butTicketsButton.addEventListener('click', () => {
+    counter++;
+    ticketBought.textContent = `${counter} Tickets Bought`
+})
+
+commentForm.addEventListener('submit', e => {
+    e.preventDefault();
+    addComment(commentInput.value)
+})
+
+fetch('http://localhost:3000/current-exhibits')
+.then(resp => resp.json())
+.then(art => {
+    art.forEach(artpiece => {
+        showArt(artpiece)
+    
+    })
+
+
+    // for (let i = 0; i < artpiece['comments'].length; i++) {
+    //     p += artpiece['comments'][i] + "<br>";
+    //   }
+})
+
